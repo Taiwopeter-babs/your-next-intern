e203ed98989ab8faf12ca2325dd15cdd3bfcc27c
Taiwo Babalola <babalolataiwop@gmail.com>
2023-05-24 23:57:35 +0100

    Add comments

--- web_app/views/views.py
+++ web_app/views/views.py
@@ -20,41 +20,49 @@ def intern_profile(intern_id):
 @app_views.route("/all_companies", methods=['GET'])
 def all_companies():
     """ Get all the companies on the platform. 
-    The template rendered depends on whether the user is
-    authenticated and the class, if authenticated.
+    The template rendered depends on the class and if the user is
+    authenticated or not.
 
-    The template rendering is checked for the `Company` class
-    to allow only users with `Intern` class apply to companies.
-
+    The template is rendered to allow only users with `Intern` 
+    class to apply to companies.
     `Company` class clients and users that are not authenticated can
     also view companies but cannot apply.
+
+    The Company objects rendered for authenticated `interns` have their
+    `application_open` set to True
     """
     from models import storage
     
     all_companies = storage.all(Company).values()
     companies = sorted(all_companies, key=lambda k: k.name)
 
-    # Get the user's class if authenticated
+    """
+    Get the user's class if authenticated. This will be used
+    to check the link between an `intern` and a `company`
+    """
     if current_user.is_authenticated:
         user = storage.get_user_by_id(current_user.id)
         user_class = user.to_dict()['__class__']
 
+        # Only render companies whose application_open status is True
+        open_com = [com for com in companies if com.application_open]
         if user_class == 'Intern':
             return render_template("auth_all_companies.html",
-                                   companies=companies)
+                                   companies=open_com)
     return render_template("all_companies.html", companies=companies)
+
 
 @app_views.route("/org_profile/<company_id>", methods=['GET', 'POST'])
 @login_required
 def company_profile(company_id):
     """ Retrieve data for a company's profile """
     from models import storage
-    if current_user.is_authenticated:
+    
+    com_obj = storage.get(Company, company_id)
+    com_interns = sorted(com_obj.interns, key=lambda k: k.first_name) 
+    return render_template("org_profile.html", user=current_user,
+                            com_interns=com_interns)
 
-        com_obj = storage.get(Company, company_id)
-        com_interns = sorted(com_obj.interns, key=lambda k: k.first_name) 
-        return render_template("org_profile.html", user=current_user,
-                               com_interns=com_interns)
 
 @app_views.route("/all_interns", methods=['GET'])
 def all_interns():
